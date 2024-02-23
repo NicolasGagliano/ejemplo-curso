1000e948026a4d19db05f445ec3319b5298ad190
Nicolas Gagliano <nicolasgagliano@hotmail.com>
2024-02-23 18:07:05 -0300

    primera subida

---
+++ Desafio1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string l;
+            //Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla el estado del Telefono.
+
+            Telefono t1 = new Telefono("MK1","Samsung");
+
+            Telefono t2 = new Telefono("S50", "Nokia");
+
+            Console.WriteLine("Ingrese el numero de telefono del T1:");
+            t1.NumeroTelefonico = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el codigo de Operador del T1:");
+            t1.CodigoOperador = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero de telefono del T2:");
+            t2.NumeroTelefonico = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el codigo de Operadordel T1:");
+            t2.CodigoOperador = int.Parse(Console.ReadLine());
+
+            t1.Marca = "Nokia";
+
+            Console.WriteLine("La informacion del primer telefono es: /n Marca: " + t1.Marca + " numero: " + t1.NumeroTelefonico);
+
+            
+
+          
+
+            t1.Llamar("Smeagol");
+
+
+            Console.WriteLine(t1.Llamar());
+
+            Console.WriteLine(t1.Llamar("Smeagol"));
+
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Desafio1/Telefono.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Telefono
+    {
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+
+        public Telefono (string modelo, string marca)
+        {
+            this.Modelo = modelo;
+            this.Marca = marca;
+        }
+
+        public string Modelo { get; }
+
+        public string Marca { get; set; }
+
+        public int NumeroTelefonico { get; set; }
+
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+
+            set
+            {
+                if (value>0 && value <4)
+                    codigoOperador = value;
+                else
+                    codigoOperador = 0;
+            }
+        }
+
+        //Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
+
+        public string Llamar ()
+        {
+           
+            return "Realizando llamada...";
+        }
+        //Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string con la leyenda "Llamando a " + contacto
+        public string Llamar (string personaje)
+        {
+            return "Llamando a " + personaje;
+
+        }
+
+
+
+    }
+}
